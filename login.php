941c44c08c6fb62ceb1c6516f1280868fe5634bd
EpikSushi21 <huangw11@miamioh.edu>
2019-04-25 01:15:44 -0400

    got the html back

--- login.php
+++ login.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-  <link rel="stylesheet" type="text/css" href="css/signUpForm.css"/>
-<html>
-<form  method="POST" action='login.php'>
-<div class="container">
-	<div align="center">
-  <h1>Log in</h1>
-  <p>Please fill the information to login.</p>
-  <hr>
-  <label for="usrname"><b>User Name</b></label>
-  <input type="text" placeholder="Enter User Name" name="usrname" required>
-
-  <label for="psw"><b>Password</b></label>
-  <input type="password" placeholder="Enter Password" name="psw" required>
-	<input type="button" value="Cancel" onclick="javascrtpt:window.location.href='nbaWebsite.html'">
-	<input type="submit" name="submit" value="submit" >
-
-</div>
-</div>
-
-</form>
-</html>
 <?php
 if(!isset($_POST['submit'])){
 	exit();
